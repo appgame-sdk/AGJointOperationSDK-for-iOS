bdbe5c6c4a6dfd6a2d8abcd887bc3f8afae706eb
Hunter <hunter_mz@icloud.com>
2017-02-21 18:42:39 +0800

    1.2.1

--- AGJointOperationSDK/AGJointOperationSDK.framework/Headers/AGConstants.h
+++ AGJointOperationSDK/AGJointOperationSDK.framework/Headers/AGConstants.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#define AGJOINTOPERATIONSDK_VERSION @"1.1.6"
+#define AGJOINTOPERATIONSDK_VERSION @"1.2.1"
 
 ///--------------------------------------
 #pragma mark - Notification
